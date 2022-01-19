4d81cf9954c0475e09fb096117a09c8c13b77979
unix4 <69163299+unix4@users.noreply.github.com>
2022-01-19 16:29:52 +0100

    Create HelloWorldMessageProvider.java

---
+++ src/com/apurav/loosecoupling/service/HelloWorldMessageProvider.java
@@ -0,0 +1,9 @@
+package com.apurav.loosecoupling.service;
+
+public class HelloWorldMessageProvider implements IMessageProvider {
+
+	@Override
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
